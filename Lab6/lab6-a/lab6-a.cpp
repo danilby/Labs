eef53ad61a9ef6fccf576c822a7ff77da1d5b21f
danilby <danilby37@gmail.com>
2021-10-20 00:09:01 +0300

    upgrade

--- Lab6/lab6-a/lab6-a.cpp
+++ Lab6/lab6-a/lab6-a.cpp
@@ -3,17 +3,19 @@
 #include <cstdlib>
 #include <ctime>
 #include "windows.h"
-int main()
+int main()   
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int x, y;
+    int x, a1, a2, y;
+    a1 = 1;
+    a2 = 100;
     int n = 1;
     printf("Добрый день, поиграем в игру!\n");
     printf("Я загадываю число от 1 до 100 ваша задача угадать\n");
     srand(time(0));
-    x = rand() % 100 + 1; // случайное число от m до n-1 + m где  n = 100 m = 1
+    x = rand() % a2 + a1; // случайное число от m до n-1 + m где  n = 100 m = 1
     printf("Я загадал, поехали >");
     do
     {
@@ -22,15 +24,54 @@ int main()
             break;
         }
         if (y > x) {
-            printf("Меньше >");
+            printf("Меньше > ");
             n++;
         }
         else {
-            printf("Больше >");
+            printf("Больше > ");
             n++;
         }
     } while (y != x);
     printf("Поздравляю вы угадали!\n");
     printf("Всего попыток было: %d\n\n\n", n);
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //int L = 1;
+    //int R = 100;
+    //int mid;
+    //int k,x;
+    //int n = 1;
+    //printf("Добрый день, поиграем в игру!\n");
+    //printf("Я загадываю число от 1 до 100 ваша задача угадать\n");
+    //srand(time(0));
+    //x = rand() % R + L; // случайное число от m до n-1 + m где  n = 100 m = 1
+    //printf("Я загадал, поехали >");
+    //do {
+    //    scanf_s("%d", &k);
+    //    L = 1, R = 100;
+    //    while (L <= R)
+    //    {
+    //        mid = (L + R) / 2;
+    //        if (mid == k) break;
+    //        if (mid > k) R = mid - 1;
+    //        else L = mid + 1;
+    //    }
+    //    if (mid == x) break;
+    //    if (mid > x) printf("Меньше > "), n++;
+    //    else printf("Больше > "), n++;
+
+    //} while (x != mid);
+    //printf("Поздравляю вы угадали!\n");
+    //printf("Всего попыток было: %d\n\n\n", n);
     
 }
--- Lab6/lab6-b/lab6-b.cpp
+++ Lab6/lab6-b/lab6-b.cpp
@@ -7,13 +7,13 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int x, y, z;
+    int x, y, z, i, flag;
     int n = 0;
     printf("Сегодня, сдаем тест на знание таблицы умножения\n");
     printf("Всего будет 10 примеров\n");
     printf("Оценивается по 10-бальной шкале, и так, начнем\n");
     srand(time(0));
-    for (int i = 0; i < 10;i++)
+    for (i = 0; i < 8;i++)
     {
         x = rand() % 9 + 1;
         y = rand() % 9 + 1;
@@ -23,14 +23,18 @@ int main()
             n++;
     }
     printf("Всего правильных ответов: %d\n", n);
-    if (n <= 5) {
-        if (n == 5)
+    if (i % 2 != 0)
+        flag = 1;
+    else
+        flag = 0;
+    if ((flag == 0 && n <= i / 2) || (flag == 1 && n <= (i + 1) / 2)) {
+        if ((flag == 0 && n == i / 2) || (flag == 1 && n == (i + 1) / 2))
             printf("Можно и лучше\n\n\n");
         else
             printf("На пересдачу\n\n\n");
     }
-    if (n > 5) {
-        if (n == 10)
+    if ((flag == 0 && n > i / 2) || (flag == 1 && n > (i + 1) / 2)) {
+        if (n == i)
             printf("Отлично\n\n\n");
         else
             printf("Хороший результат\n\n\n");
